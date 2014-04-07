44c6552d9822530fbde4c96f6b4c1883384351b2
Canfrère Nicolas <nico.canfrere@gmail.com>
2014-04-07 11:11:28 +0200

    modif gitconfig

--- web/survey/index.php
+++ web/survey/index.php
@@ -1,46 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Nicolas
- * Date: 01/04/2014
- * Time: 23:19
- */
-
-?>
-<!doctype html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="fr"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="fr"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang="fr"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="fr"> <!--<![endif]-->
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <title>Statistiques sorties des enquêtes</title>
-    <link rel="stylesheet" href="/css/survey.css"/>
-    <link rel="stylesheet" href="/css/font-awesome.min.css"/>
-    <script src="/js/vendor/modernizr.js"></script>
-</head>
-<body>
-<!-- if html.lt-ie9 -->
-<div class="browserhappy">
-    <p>Vous utilisez une version d'internet explorer incompatible avec ce site. Mettez à jour internet explorer vers la version 11, ou utilisez des navigateurs tels que
-        <a href="https://www.google.com/intl/fr/chrome/browser/">Google Chrome</a>, <a href="http://www.mozilla.org/en-US/firefox/all/">Firefox</a>,...</p>
-</div>
-
-<div id="topbar">
-
-</div>
-<div id="sidebar">
-
-</div>
-
-<div id="main">
-
-</div>
-
-
-<script src="/js/apps/survey/main.js"></script>
-</body>
-</html>
+ * User: Nicolas Canfrère
+ * Date: 07/04/2014
+ * Time: 11:05
+ */ 
